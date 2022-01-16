f13d1fc2152fce1351de8b5877c21645951447a9
jonathan <jonodrew@users.noreply.github.com>
2022-01-16 14:33:25 +0000

    Bump version: 0.1.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="mentor-match",
-    version="0.1.0",
+    version="1.0.0",
     description="A graphical interface to the mentor-match package",
     long_description=README,
     long_description_content_type="text/markdown",
